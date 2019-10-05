c1f660106359f36b28cf09f27fd25653e08f2217
Wilson Neto <contato@wilsonneto.com.br>
2019-10-05 08:26:41 -0300

    work in progress: dashboard

--- src/pages/Dashboard/index.js
+++ src/pages/Dashboard/index.js
@@ -1,16 +1,33 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import api from '../../services/api';
 
+import './styles.css';
+
 export default function() {
+  const [spots, setSpots] = useState([]);
+
   useEffect(() => {
     (async function loadSpots() {
       const user_id = localStorage.getItem('user');
       const response = await api.get('/dashboard', {
         headers: { user_id }
       });
-      console.log(response.data);
+
+      setSpots(response.data);
     })();
   }, []);
 
-  return <>Dashboard...</>;
+  return (
+    <ul className="spot-list">
+      {spots.map(spot => (
+        <li key={spot._id}>
+          <header
+            style={{ backgrounImage: `url(${spot.thumbnail_url})` }}
+          ></header>
+          <strong>{spot.company}</strong>
+          <span>{spot.price}</span>
+        </li>
+      ))}
+    </ul>
+  );
 }
